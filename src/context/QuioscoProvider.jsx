8c9b3e280dab8c070868f0329d7bcf6a56e2e6ac
Andrea Lucero <alucerotrasvina@gmail.com>
2024-05-29 14:25:16 -0700

    Url como variable de entorno

--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -35,7 +35,11 @@ const QuioscoProvider = ({children}) => {
     //Se trae la respuesta tipo json de laravel del nombre de la categoria y su icono
     const obtenerCategorias = async () => {
         try{
-            const {data} = await axios('http://127.0.0.1:8000/api/categorias')
+
+            //Variable de entorno local
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/api/categorias`);
+
+            
             setCategorias(data.data)
             setCategoriaActual(data.data[0])
         }catch (error){
