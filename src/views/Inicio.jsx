0dc170a6074c7043a3dc9bf313ab5a4858514deb
Andrea Lucero <alucerotrasvina@gmail.com>
2024-05-29 16:29:37 -0700

    consumir api de productos

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,6 +1,7 @@
-import { productos as data, productos } from '../data/productos'
+import useSWR from 'swr'
 import Producto from '../components/Producto'
 import useQuiosco from '../hooks/useQuiosco'
+import clienteAxios from '../config/axios'
 
 export default function Inicio() {
 
@@ -8,8 +9,20 @@ export default function Inicio() {
   //Accedemos al value del QuioscoProvider
   const { categoriaActual } = useQuiosco()
 
+  //consulta SWR
+  const fetcher = () => clienteAxios('/api/productos').then(data => data.data)
+  const { data, error, isLoading } = useSWR('/api/productos', fetcher, {
+    refreshInterval: 1000
+  })
+
+  // console.log(data)
+  // console.log(error)
+  // console.log(isLoading)
+
+  if(isLoading) return 'Cargando...';
+
   //De esta forma filtramos jeje, que se muestre a lo que se haga clicl
-  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+  const productos = data.data.filter(producto => producto.categoria_id === categoriaActual.id)
 
   // console.log(productos)
 
